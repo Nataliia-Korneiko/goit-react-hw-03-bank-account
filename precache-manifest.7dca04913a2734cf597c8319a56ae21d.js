69cb0589ccd078bebf801ce5b73e276bf0961c5d
Nataliia Korneiko <korneyko_ns@ukr.net>
2019-11-26 00:12:59 +0200

    Updates

---
+++ precache-manifest.7dca04913a2734cf597c8319a56ae21d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7357b2e488bbeb57d91c1054dddd99",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "93136de786aaa1e96c75",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "744004d079415e9b3c50",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.d0a3a5ec.chunk.css"
+  },
+  {
+    "revision": "93136de786aaa1e96c75",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.ebb67934.chunk.js"
+  },
+  {
+    "revision": "744004d079415e9b3c50",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.5e523e15.chunk.js"
+  },
+  {
+    "revision": "0c18147979c7c79bba04",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.eaa3779c.js"
+  }
+]);
